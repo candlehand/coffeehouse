55a632e77ee56b5a1f0cf7c096eff9ed3265af60
Chip Weatherly <chip.weatherly@pcc.edu>
2025-01-07 11:31:36 -0800

    First draft of timer- working, no errors

--- coffeehouse/app/src/main/java/com/example/coffeehouse/MainActivity.kt
+++ coffeehouse/app/src/main/java/com/example/coffeehouse/MainActivity.kt
@@ -5,16 +5,38 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.os.CountDownTimer
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var textView : TextView
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        textView = findViewById(R.id.textView)
+
+        // testing : (findViewById(R.id.main) -> (findViewById(R.id.textView)
+        //ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.textView)) { v, insets ->
+        //    val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+        //    v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+        //    insets
+        //}
+
+
+        // Timer object, 600000 = 10 mins 1 = 1 millisecond
+        object : CountDownTimer(30000, 1000) {
+
+            // Callback function triggered regularly
+            override fun onTick(millisUntilFinished: Long) {
+                textView.text = "seconds remaining: " + millisUntilFinished / 1000
+            }
+
+            // fires when the time is up
+            override fun onFinish() {
+                textView.text = "Done!"
+            }
+        }.start()
     }
 }
