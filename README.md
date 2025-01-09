08538065e1136f951de1534764a9d843d4513404
Chip Weatherly <weatherly.chip@gmail.com>
2025-01-08 16:05:37 -0800

    Clocks now correctly record time between swaps

---
+++ README.md
@@ -0,0 +1,2 @@
+# coffeehouse
+Coffeehouse- Adjective used to describe a move, player, or style of play characterized by risky, positionally dubious play that sets traps for the opponent. This is an android chess clock/timer.
--- coffeehouse/app/src/main/java/com/example/coffeehouse/MainActivity.kt
+++ coffeehouse/app/src/main/java/com/example/coffeehouse/MainActivity.kt
@@ -6,6 +6,7 @@ Purpose: Holds logic for chess clock app main screen
 Changelog:
 1/6/25 Initial creation - CW
 1/7/25 Added clock functionality - CW
+1/8/25 Added ability to switch between clocks by clicking the screen - CW
  */
 package com.example.coffeehouse
 
@@ -18,55 +19,16 @@ import android.widget.TextView
 import java.text.DecimalFormat
 
 class MainActivity : AppCompatActivity() {
-
-    // holds user selection of which player's turn it is'
-    var timerSelected = 1
-
-    // inits textView variables for the 2 clocks
+    // hold user's selection of which player's turn it is
+    private var timerSelected = 1
+    // holds remaining time on each clock
+    private var clock1Time: Long = 600000
+    private var clock2Time: Long = 600000
+    // init textView variables for the 2 clocks
     lateinit var clock1 : TextView
     lateinit var clock2 : TextView
-
-    // Timer object, 600000 = 10 mins 1 = 1 millisecond
-    val timer1 = object : CountDownTimer(600000, 1000) {
-
-        // Callback function triggered regularly
-        override fun onTick(millisUntilFinished: Long) {
-
-            val dec = DecimalFormat("00")
-
-            val hour = (millisUntilFinished / 3600000) % 24
-            val min = (millisUntilFinished / 60000) % 60
-            val sec = (millisUntilFinished / 1000) % 60
-            clock1.text = dec.format(hour) + ":" + dec.format(min) + ":" + dec.format(sec)
-        }
-
-        // fires when the time is up
-        override fun onFinish() {
-            clock1.text = getString(R.string.timeOut)
-        }
-    }
-
-    val timer2 = object : CountDownTimer(600000, 1000) {
-
-        // Callback function triggered regularly
-        override fun onTick(millisUntilFinished: Long) {
-
-            val dec = DecimalFormat("00")
-
-            val hour = (millisUntilFinished / 3600000) % 24
-            val min = (millisUntilFinished / 60000) % 60
-            val sec = (millisUntilFinished / 1000) % 60
-            clock2.text = dec.format(hour) + ":" + dec.format(min) + ":" + dec.format(sec)
-        }
-
-        // fires when the time is up
-        override fun onFinish() {
-            clock2.text = getString(R.string.timeOut)
-        }
-    }
-
+    // runs on creation of the main activity, at app start
     override fun onCreate(savedInstanceState: Bundle?) {
-
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
@@ -74,26 +36,57 @@ class MainActivity : AppCompatActivity() {
         clock1 = findViewById(R.id.clock1)
         clock2 = findViewById(R.id.clock2)
         // assign initial values to clock text. To be changed into variables
-        clock1.text = "10:00:00"
-        clock2.text = "10:00:00"
+        clock1.text = getString(R.string.initialClock)
+        clock2.text = getString(R.string.initialClock)
         // begin the countdown!!!
-        timerInit(timer1, timer2, timerSelected)
+        startTimers(timer1, timer2, timerSelected)
+    }
+    // Timer object for player 1, 600000 = 10 minutes 1 = 1 millisecond
+    private val timer1 = object : CountDownTimer(clock1Time, 1000) {
+        // Callback function triggered regularly
+        override fun onTick(millisUntilFinished: Long) {
+            clock1Time -= 1000
+            val dec = DecimalFormat("00")
+            val hour = (clock1Time / 3600000) % 24
+            val min = (clock1Time / 60000) % 60
+            val sec = (clock1Time / 1000) % 60
+            clock1.text = dec.format(hour) + ":" + dec.format(min) + ":" + dec.format(sec)
+        }
+        // displays message when the time runs out
+        override fun onFinish() {
+            clock1.text = getString(R.string.timeOut)
+        }
+    }
+    // Timer object for player 2
+    private val timer2 = object : CountDownTimer(clock2Time, 1000) {
+        // Callback function triggered regularly
+        override fun onTick(millisUntilFinished: Long) {
+            clock2Time -= 1000
+            val dec = DecimalFormat("00")
+            val hour = (clock2Time / 3600000) % 24
+            val min = (clock2Time / 60000) % 60
+            val sec = (clock2Time / 1000) % 60
+            clock2.text = dec.format(hour) + ":" + dec.format(min) + ":" + dec.format(sec)
+        }
+        // displays message when the time runs out
+        override fun onFinish() {
+            clock2.text = getString(R.string.timeOut)
+        }
     }
     // method for capturing button click to swap clocks
     fun timerSwapButtonClick(view: View?) {
+        // save the current clock numbers when clicked
+        println(clock1Time)
         timerSelected = if (timerSelected == 1){
             2
         } else {
             1
         }
-        timerInit(timer1, timer2, timerSelected)
+        startTimers(timer1, timer2, timerSelected)
     }
 }
-
-
-
 // method initializes one timer at a time at program start
-fun timerInit(timer1:CountDownTimer, timer2:CountDownTimer, timerSelected: Int) {
+fun startTimers(timer1:CountDownTimer, timer2:CountDownTimer, timerSelected: Int) {
     // 'timerSelected' will hold the user's choice of who will go first
     if (timerSelected == 1){
         timer2.cancel()
@@ -102,5 +95,4 @@ fun timerInit(timer1:CountDownTimer, timer2:CountDownTimer, timerSelected: Int) 
         timer1.cancel()
         timer2.start()
     }
-
 }
